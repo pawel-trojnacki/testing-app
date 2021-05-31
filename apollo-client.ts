42f78bc121bd18e74119eec2323e1e3e21fb5342
Paweł Trojnacki <69036215+pawel-trojnacki@users.noreply.github.com>
2021-05-31 14:07:06 +0200

    Update apollo-client.ts

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://api.testingwebsite.pl/graphql',
+  uri: 'https://api.testingwebsite.pl/graphql',
   cache: new InMemoryCache(),
 });
 
